43e93589703feadc8621bf7c14d2d3c415684102
C3NZ <cenz@cenz.io>
2019-01-23 18:57:40 -0800

    [feat] add eslint, and fix server.js formatting

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 if (!process.env.PORT) {
-  require('dotenv').config()
-  process.env.NODE_ENV = "dev"
+    require('dotenv').config();
+    process.env.NODE_ENV = "dev";
 }
 
 const express = require('express');
@@ -9,11 +9,12 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-const methodOverride = require('method-override')
+const methodOverride = require('method-override');
 
 const app = express();
 
 const mongoose = require('mongoose');
+
 mongoose.connect('mongodb://localhost/petes-pets');
 
 // view engine setup
@@ -21,10 +22,10 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // override with POST having ?_method=DELETE or ?_method=PUT
-app.use(methodOverride('_method'))
+app.use(methodOverride('_method'));
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -36,20 +37,20 @@ require('./routes/pets.js')(app);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;

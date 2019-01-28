e2372dd8ccb5c7777cfae2cf1006aff9ed20b5a4
C3NZ <cenz@cenz.io>
2019-01-27 17:51:36 -0800

    [feat] install dependencies, add the skeleton for pagination to the view and plugin the mongoose middleware for pagination functionallity

--- models/pet.js
+++ models/pet.js
@@ -1,19 +1,45 @@
-"use strict";
+// Imports
+const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate');
 
-const mongoose = require('mongoose'),
-        Schema = mongoose.Schema;
+// Import Schema from mongoose
+const { Schema } = mongoose;
 
+// Limit the amount of records to return per query
+mongoosePaginate.paginate.options = {
+    limit: 3,
+};
+
+// Create the pet schema
 const PetSchema = new Schema({
-    name            : { type: String, required: true }
-  , species         : { type: String }
-  , birthday        : { type: Date }
-  , picUrl          : { type: String }
-  , picUrlSq        : { type: String }
-  , favoriteFood    : { type: String }
-  , description     : { type: String }
+    name: {
+        type: String,
+        required: true,
+    },
+    species: {
+        type: String,
+    },
+    birthday: {
+        type: Date,
+    },
+    picUrl: {
+        type: String,
+    },
+    picUrlSq: {
+        type: String,
+    },
+    favoriteFood: {
+        type: String,
+    },
+    description: {
+        type: String,
+    },
 },
 {
-  timestamps: true
+    timestamps: true,
 });
+
+// Enable mongoose paginate for the pet schema
+PetSchema.plugin(mongoosePaginate);
 
 module.exports = mongoose.model('Pet', PetSchema);

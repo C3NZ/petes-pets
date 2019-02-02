428ab3582ef66ea0426343dafb29185a99a87b57
C3NZ <cenz@cenz.io>
2019-02-02 14:55:35 -0800

    [feat] add a payment route to allow users to make purchases on specific animals

--- routes/pets.js
+++ routes/pets.js
@@ -2,10 +2,15 @@ const express = require('express');
 const Pet = require('../models/pet');
 const multer = require('multer');
 const Uploader = require('s3-uploader');
+const stripe = require('stripe')(process.env.PRIVATE_STRIPE_API_KEY);
+
 
 // Middleware for handling multipart data
 const upload = multer({ dest: 'uploads/' });
 
+const router = express.Router();
+
+// Instantiate client for uploading to amazon s3
 const client = new Uploader(process.env.S3_BUCKET, {
     aws: {
         path: 'pets/avatar',
@@ -18,38 +23,42 @@ const client = new Uploader(process.env.S3_BUCKET, {
         versions: true,
         original: true,
     },
-    versions: [
-        {
-            maxWidth: 400,
-            aspect: '16:10',
-            suffix: '-standard',
-        },
-        {
-            maxWidth: 300,
-            aspect: '1:1',
-            suffix: '-square',
-        },
+    versions: [ {
+        maxWidth: 400,
+        aspect: '16:10',
+        suffix: '-standard',
+    },
+    {
+        maxWidth: 300,
+        aspect: '1:1',
+        suffix: '-square',
+    },
     ],
 });
-
-const router = express.Router();
 
 // NEW PET
 router.get('/new', (req, res) => {
     res.render('pets-new');
 });
 
+// SEARCH FOR A NEW PET
 router.get('/search', (req, res, next) => {
     const term = new RegExp(req.query.term, 'i');
-    // Allow users to search for both dog name and species
     const currentPage = req.query.page || 1;
 
+    // Allow users to search for both dog name and species
     Pet
         .paginate(
             { $or: [{ name: term }, { species: term }] },
             { page: currentPage },
         )
         .then((results) => {
+            // Attach the found pets, current count, and the 
+            res.locals.pets = results.docs;
+            res.locals.pageCount = results.pages
+            res.locals.currentPage = currentPage;
+            res.locals.term = req.query.term;
+
             res.render('pets-index', {
                 pets: results.docs,
                 pageCount: results.pages,
@@ -94,7 +103,12 @@ router.get('/:id', (req, res) => {
     Pet
         .findById(req.params.id)
         .exec((err, pet) => {
-            res.render('pets-show', { pet });
+            // Attach the pet and public stripe api key
+            res.locals.pet = pet;
+            res.locals.PUBLIC_STRIPE_API_KEY = process.env.PUBLIC_STRIPE_API_KEY; 
+
+            // Render the single pet
+            res.render('pets-show', res.locals);
         });
 });
 
@@ -140,6 +154,28 @@ router.delete('/:id', (req, res) => {
         .exec((err, pet) => {
             return res.redirect('/')
         });
+});
+
+router.post('/:id/purchase', (req, res, next) => {
+    const token = req.body.stripeToken;
+
+    // Create the official charge
+    Pet
+        .findOne({ _id: req.params.id })
+        .exec((err, pet) => {
+            if (err) return next(err);
+
+            stripe
+                .charges
+                .create({
+                    amount: pet.price * 100,
+                    currency: 'usd',
+                    description: `Purchased ${pet.name}, ${pet.species}`,
+                    source: token,
+                })
+                .then(charge => res.redirect(`/pets/${req.params.id}`))
+                .catch( err => next(err))
+        })
 });
 
 module.exports = router;
